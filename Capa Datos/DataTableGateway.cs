b7fd464cb41b4e76bead2c108d7ecce0701b535f
diegoinsua <diegoinsua@hotmail.com>
2013-10-04 21:01:49 -0300

    Inicial

---
+++ Capa Datos/DataTableGateway.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Clinica_frba.CapaDatos
+{
+    abstract class DataTableGateway
+    {
+        dataset  
+    }
+}
